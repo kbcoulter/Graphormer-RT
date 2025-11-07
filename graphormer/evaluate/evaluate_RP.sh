71a7cbd0057758a185c9e3c25bd506cd8bf5b03e
kbcoulter <coulterkb@yahoo.com>
2025-11-06 22:19:38 -0800

    Updated pred squeezing in evaluate.py -> should be working

---
+++ graphormer/evaluate/evaluate_RP.sh
@@ -0,0 +1,22 @@
+cd ./deep_metab/Graphormer-RT/graphormer/evaluate/
+
+python evaluate.py \
+    --user-dir ../../graphormer \
+    --num-workers 32 \
+    --ddp-backend=legacy_ddp \
+	--user-data-dir rp_test \
+	--dataset-name RT_test \
+    --task graph_prediction \
+	--criterion rmse \
+	--arch graphormer_base \
+    --encoder-layers 8 \
+    --encoder-embed-dim  512 \
+    --encoder-ffn-embed-dim 512 \
+    --encoder-attention-heads 64 \
+    --freeze-level -4 \
+    --mlp-layers 5 \
+    --batch-size 64 \
+    --num-classes 1 \
+    --save-path '../../../predictions/RP_preds.csv' \
+    --save-dir '/workspace/Graphormer-RT/checkpoints/' \
+    --split train \
